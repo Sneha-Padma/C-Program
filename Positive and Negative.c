1e8056a309e8893e406c5a38790331a13baf5cfc
Sneha-Padma <128279580+Sneha-Padma@users.noreply.github.com>
2023-03-20 20:25:43 +0530

    Positive and Negative Number

---
+++ Positive and Negative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("enter a number");
+	scanf("%d",&a);
+	if(a>0)
+	printf("the numbver is positive ");
+	else if(a<0)
+	printf("the number is negative");
+	else
+	printf("number is zero ");
+	return 0;
+	
+}
